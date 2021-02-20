7e91ffabce34425e6d22ca666acd6650d2c076c6
SEKI-YUTA <zard.since1991@gmail.com>
2021-02-20 12:50:46 +0900

    no.2

---
+++ public/register.php
@@ -0,0 +1,43 @@
+<?php
+  $err = [];
+
+  if(! $username = filter_input(INPUT_POST, 'username')) {
+    $err[] = "ユーザー名を記入してください";
+  }
+
+  if(! $email = filter_input(INPUT_POST, 'email')) {
+    $err[] = "メールアドレスを記入してください";
+  }
+  $password = filter_input(INPUT_POST,"password");
+  if(!preg_match("/\A[a-z\d]{8,100}+\z/i",$password)) {
+    $err[] = 'パスワードは英数字８文字以上１００文字以下にしてください';
+  }
+  $password_conf = filter_input(INPUT_POST,"password_conf");
+  if($password !== $password_conf) {
+    $err[] = '確認用パスワードと異なって居ます';
+  }
+
+  if(count($err) === 0) {
+    //登録処理
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ユーザー登録完了画面</title>
+</head>
+<body>
+<?php if(count($err) > 0) : ?>
+  <?php foreach($err as $e) : ?>
+    <p><?php echo $e ?></p>
+  <?php endforeach; ?>
+<?php else : ?>
+  <p>ユーザー登録が完了しました</p>
+<?php endif; ?>
+  <a href="./signup.php">戻る</a>
+</body>
+</html>
